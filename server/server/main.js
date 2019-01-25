c43f791d6513813a06049b520fd218e624a35b89
Yannic Kilcher <yannic.kilcher@gmail.com>
2019-01-25 11:38:16 +0100

    stuff

--- server/server/main.js
+++ server/server/main.js
@@ -1,10 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import CpuStats from '/imports/api/CpuStats';
 import '/imports/api/server/publications';
-
-function insertLink(title, url) {
-  CpuStats.insert({ title, url, createdAt: new Date() });
-}
 
 Meteor.startup(() => {
 });
