561c8c9a885f910355829c530a17d046ecd3e3e6
Yannic Kilcher <yannic.kilcher@gmail.com>
2019-01-26 10:19:26 +0100

    sorting machines

--- server/imports/ui/StatsArea.jsx
+++ server/imports/ui/StatsArea.jsx
@@ -24,7 +24,7 @@ class StatsArea extends Component {
       <div>
         <h1>GPU Stats</h1>
         <MachineContainerDiv>
-          {machines.map(m => 
+          {machines.sort().map(m => 
             <Machine machine={m._id} key={m._id}/>
           )}
         </MachineContainerDiv>
